dafb3d4b605b9dba6f59940603efcf49e1ff9e8a
Harrison Chen <harrisonchen@Harrisons-MacBook-Air.local>
2019-09-18 10:52:39 +0800

    First Change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1> This is Willy the Whale</h1>
+        <img alt= "Whale"
+        src= "https://i.cbc.ca/1.5202436.1563993694!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/killer-whale.jpg"></img>
+<h2>Moby Dick 2.0</h2>
+        <p> 
+From a young age Willy the Whale swam in the ocean. One day, he meet Bob the Builder
+</p>
+
+       <div>
+         <img alt= "Bob the Builder"
+         src= "http://www.bobthebuilder.com/Resources/img/for-kids-bg.jpg"></img>
+       </div>
       </header>
     </div>
   );
