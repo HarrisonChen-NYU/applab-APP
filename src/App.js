4f566bc02a14facebb28cc7714e73ab911a2b99d
Harrison Chen <harrisonchen@Harrisons-MacBook-Air.local>
2019-09-24 13:00:29 +0800

    First Change

--- src/App.js
+++ src/App.js
@@ -3,23 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1> This is Willy the Whale</h1>
-        <img alt= "Whale"
-        src= "https://i.cbc.ca/1.5202436.1563993694!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/killer-whale.jpg"></img>
-<h2>Moby Dick 2.0</h2>
-        <p> 
-From a young age Willy the Whale swam in the ocean. One day, he meet Bob the Builder
-</p>
-
-       <div>
-         <img alt= "Bob the Builder"
-         src= "http://www.bobthebuilder.com/Resources/img/for-kids-bg.jpg"></img>
+        <div className="App">
+        <header>
+        <h1> Six Layers of Stack </h1>
+            <h2>Harrison Chen App Lab Assignment</h2>
+             <h3> User Level</h3>
+             <img src="https://static2.bigstockphoto.com/0/5/2/large1500/250634407.jpg"></img>  
+             <p>
+              At the user level, I personally feel like the mobile device mandates a very extended duration in my life. It is not uncommon for me to first grab my phone when I wake up in the morning and also be the last thing I hold before I go to bed. This immense over reliance on my celluar device has many benefits but also many more negatives.
+              </p>
+               <h3>Interface Level</h3>
+              <img src="https://i.ytimg.com/vi/743jq0ncVtA/maxresdefault.jpg" alt= "interface Level"></img>
+             <p>
+              From the interface level, my most time consuming application is Youtube which is a video sharing application that provides an immense pleathora of entertainment and educational content availible.  It is very diffiuclt to resist the temptation of these outside influences. Furthemore, this interface caused a major problem with time management because I increase my level of procrastination and consequntly lose alot of productivity. 
+             </p>
+             <h3>Address Level</h3>
+             <img src="https://i2.wp.com/www.dignited.com/wp-content/uploads/2018/05/Wi-Fi-home.png?fit=600%2C361ssl=1" alt="Address Level"></img>
+              <p>
+              From my household, my family also utilizes the wifi and as a collective, we tend to rely alot on technology. Although my parents used to be very traditional but due to the convinience that technology brings, they have both decided to embrace the change rather than rejecting it. 
+             </p>
+              <h3> City Level</h3>
+              <img src="https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,h_463,q_50,w_800/v1/clients/vancouverbc/01014_201809_AlbertNormandin_False_CreekVancouverDowntownCityAerial_23dd1724-bfd7-4cc9-9d93-60faac3cb2fa.jpg" alt="City Level"></img> 
+             <p>
+              I am from Vancouver where people are very interested in athletics and the outdoors. We love swimming, hiking, and various other athletic events. As a result, we tend to rely less on technology but rely more on ourselves and specifically our physical capabilities. 
+             </p>
+              <h3>Cloud Level
+              </h3>
+              <img src= "https://blog.rackspace.com/wp-content/uploads/2016/08/move-your-small-business-to-the-cloud.jpg"></img>
+             <p>
+             Youtube has an immense database of data that allows for the platform to function. The amount of storage that video take is already substantial but Youtube covers so many videos that the storage area needs to be highly efficient and mechanized.
+             </p>
+             <h3> Earth Level</h3>
+             <img src="https://www.gstatic.com/earth/social/00_generic_facebook-001.jpg"></img>
+             <p>
+              Youtube is quite popular all around the world except for a few select nations. Consequently, this problem of over reliance on technology and spending too much time on the internet is a truly prevelant and far reaching complication.  
+             </p>
+          </header>
        </div>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    );
+  }
+  export default App;
